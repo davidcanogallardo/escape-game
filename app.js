9fb91b049ce2640c7fccb3ff94ce6f91723f4cba
davidcanogallardo <93164111+davidcanogallardo@users.noreply.github.com>
2021-11-01 13:08:43 +0100

    lista de amigos y notificaciones terminado

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ function rightMenu(params) {
     var list = $(".slide-menu >"+ params)
 
     if ($.trim($('.slide-menu').html()) === "") {
-        var menu = $(".right-menuu > ."+ params)
-        var a = menu.clone()
-        $('.slide-menu').append(a);
+        var menu = $(".menus > ."+ params)
+        var menuClone = menu.clone()
+        var xd = $.trim(`<div style="width: 100%;height: 100%;background: #e1e1e14a;position: absolute;top: 0;left: 0; z-index:5;" onclick="rightMenu('.`+params+`')"></div>`)
+        //pensar un nombre para la variable uwu
+        $('.slide-menu').append(menuClone).append(xd);
         $('.slide-menu > .'+ params).css("width","0%")
         $('.slide-menu > .'+ params).animate({width:"20%"}, 10)
     } else {
-        $('.slide-menu> .'+ params).animate({width:"0%"}, 10).remove()
+        $('.slide-menu > *').animate({width:"0%"}, 10).remove()
     }
 
 
