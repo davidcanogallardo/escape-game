df5357f87c558a3baa0660a1ea5f96d40c178ebe
Adnan <aaarras@ibadia.cat>
2021-11-11 20:59:20 +0100

    Trabajando en el mapa del juego

--- demo_game/game.js
+++ demo_game/game.js
@@ -1,9 +1,29 @@
-function preload(){
-    this.load.tilemapTiledJSON('map', 'assets/tilemaps/mapa.json');
-    const tileset = map.addTilesetImage('tiles', 'assets/tilesets/TSMapa/PNG');
+var map;
+var layer;
+
+function preload() {
+    game.load.tilemap('map', 'assets/tilemaps/mapa.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.image('tiles', 'assets/tilesets/TSMapa/PNG/tileset.png');  
 }
 
-function create(){
-    const map = this.make.tilemap({ key: 'map' });
-    const tileset = map.addTilesetImage('tileset', 'tiles');
-}
+
+
+function create() {
+    map = game.add.tilemap('map1');  // 
+    map.addTilesetImage('tileset', 'tiles');  // set tileset name
+    layer = map.createLayer('ground');  // set layer name
+    layer.resizeWorld();
+  }
+
+var config = {
+    type: Phaser.AUTO,
+    width: 750,
+    height: 500,
+    parent: 'game',
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+var game = new Phaser.Game(config);
