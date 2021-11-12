50b1f15c691a0ff646d777ac74c7adebecf3d17e
Adnan Aarras <aaarras@ibadia.cat>
2021-11-12 13:44:43 +0100

    Trabajando en la implantacion del mapa

--- demo_game/game.js
+++ demo_game/game.js
@@ -1,29 +1,30 @@
-var map;
-var layer;
-
-function preload() {
-    game.load.tilemap('map', 'assets/tilemaps/mapa.json', null, Phaser.Tilemap.TILED_JSON);
-    game.load.image('tiles', 'assets/tilesets/TSMapa/PNG/tileset.png');  
-}
-
-
-
-function create() {
-    map = game.add.tilemap('map1');  // 
-    map.addTilesetImage('tileset', 'tiles');  // set tileset name
-    layer = map.createLayer('ground');  // set layer name
-    layer.resizeWorld();
+const config = {
+    type: Phaser.AUTO, // Which renderer to use
+    width: 800, // Canvas width in pixels
+    height: 600, // Canvas height in pixels
+    parent: "game-container", // ID of the DOM element to add the canvas to
+    scene: {
+      preload: preload,
+      create: create,
+      update: update
+    }
+  };
+  
+  const game = new Phaser.Game(config);
+  
+  function preload() {
+    this.load.image("tiles", "assets/tilesets/TSMapa/PNG/tileset.png");
+    this.load.tilemapTiledJSON("map", "assets/tilemaps/mapa.json");
+  }
+  
+  function create() {
+    const map = this.make.tilemap({ key: "map" });
+    const tileset = map.addTilesetImage("tileset", "tiles");
+    const ground = map.createLayer("ground", tileset, 0, 0);
+    const walls = map.createLayer("walls", tileset, 0, 0);
+    const camera = this.cameras.main;
   }
 
-var config = {
-    type: Phaser.AUTO,
-    width: 750,
-    height: 500,
-    parent: 'game',
-    scene: {
-        preload: preload,
-        create: create
-    }
-};
-
-var game = new Phaser.Game(config);
+  function update(time, delta) {
+    controls.update(delta);
+  }
