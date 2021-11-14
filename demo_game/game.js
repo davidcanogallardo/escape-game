309a5e8f8d7f66c9cdb8589d8a91caf2dfca0de0
Adnan Aarras <aaarras@ibadia.cat>
2021-11-14 16:14:27 +0100

    Trabajando en el movimiento del personaje

--- demo_game/game.js
+++ demo_game/game.js
@@ -10,22 +10,78 @@ const config = {
     }
   };
   
-  const game = new Phaser.Game(config);
+game = new Phaser.Game(config);
   
-  function preload() {
-    this.load.image("tiles", "assets/tilesets/TSMapa/PNG/tileset.png");
-    this.load.tilemapTiledJSON("map", "assets/tilemaps/mapa.json");
-    this.load.atlas("player", "assets/character/player.png" , "assets/character/player.json");
-    this.cursors = this.input.keyboard.createCursorKeys();
-  }
+function preload() {
+  this.load.image("tiles", "assets/tilesets/TSMapa/PNG/tileset.png");
+  this.load.tilemapTiledJSON("map", "assets/tilemaps/mapa.json");
+  this.load.atlas("player", "assets/character/player.png" , "assets/character/player.json");
+}
   
-  function create() {
-    const map = this.make.tilemap({ key: "map" });
-    const tileset = map.addTilesetImage('dungeon', 'tiles');
-    const player = this.add.sprite(128,128, 'player');
-    map.createStaticLayer('ground', tileset);
-    map.createStaticLayer('walls', tileset);
-  }
+function create() {
+  cursors = this.input.keyboard.createCursorKeys();
+  map = this.make.tilemap({ key: "map" });
+  tileset = map.addTilesetImage('dungeon', 'tiles');
+  map.createStaticLayer('ground', tileset);
+  map.createStaticLayer('walls', tileset);
+  player = this.add.sprite(100,230, "player", "walk-down-3.png");
 
-  function update() {
+  this.anims.create({
+    key: 'player-idle-down',
+    frames: [{ key: 'player', frame: 'walk-down-3.png' }]
+  })
+
+  this.anims.create({
+    key: 'player-idle-up',
+    frames: [{ key: 'player', frame: 'walk-up-3.png' }]
+  })
+
+  this.anims.create({
+    key: 'player-idle-side',
+    frames: [{ key: 'player', frame: 'walk-side-3.png' }]
+  })
+
+  this.anims.create({
+    key: 'player-run-down',
+    frames: this.anims.generateFrameNames('player', { start: 1, end: 8, prefix: 'run-down-', suffix: '.png' }),
+    repeat: -1,
+    frameRate: 15
+  })
+
+  this.anims.create({
+    key: 'player-run-up',
+    frames: this.anims.generateFrameNames('player', { start: 1, end: 8, prefix: 'run-up-', suffix: '.png' }),
+    repeat: -1,
+    frameRate: 15
+  })
+
+  this.anims.create({
+    key: 'player-run-side',
+    frames: this.anims.generateFrameNames('player', { start: 1, end: 8, prefix: 'run-side-', suffix: '.png' }),
+    repeat: -1,
+    frameRate: 15
+  })
+}
+
+function update(){
+  speed = 100
+
+  if (cursors.left?.isDown){
+    this.anims.play('player-run-side', true)
+    this.setVelocity(-speed, 0)
+
   }
+  else if (cursors.right?.isDown){
+    this.anims.play('player-run-side', true)
+    this.setVelocity(speed, 0)
+
+  }
+  else if (cursors.up?.isDown){
+    this.anims.play('player-run-up', true)
+    this.setVelocity(0, -speed)
+  }
+  else if (cursors.down?.isDown){
+    this.anims.play('player-run-down', true)
+    this.setVelocity(0, speed)
+  }
+}
