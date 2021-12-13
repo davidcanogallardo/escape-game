2a2fd72bb02ad55774de57f1ebe0da1bfcec56cd
Alex <agallardo@ibadia.cat>
2021-12-13 20:38:56 +0100

    Continuacion de la waiting Room

--- src/components/waiting-room.js
+++ src/components/waiting-room.js
@@ -2,7 +2,7 @@ Vue.component('waiting-room', {
     template: //html
     `
     <div class="waitingRoom">
-        <h1>waitingRoom</h1>
+        <h1>waiting Room</h1>
         <br>
         <p>1 / 2 Players</p>
         <div class="col-3">
