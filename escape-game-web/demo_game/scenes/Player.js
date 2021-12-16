47a79aa28662e2211f7afd1ef9c644e468a9af7b
David <93164111+davidcanogallardo@users.noreply.github.com>
2021-12-16 19:09:28 +0100

    Update Player.js

--- escape-game-web/demo_game/scenes/Player.js
+++ escape-game-web/demo_game/scenes/Player.js
@@ -1,6 +1,6 @@
 class Player extends Phaser.GameObjects.Sprite{
-    constructor(scene) {
-        super(scene, 100, 250, 'player')
+    constructor(scene x, y, sprite) {
+        super(scene, x, y, sprite)
         scene.add.existing(this)
         this.setDepth(5)
         this.cursors = scene.input.keyboard.createCursorKeys();
