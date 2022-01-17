5297fd1144e61ebffb6c9d70c47765cbd19d429c
Oscar Adell <oadell@ibadia.cat>
2022-01-17 15:21:59 +0100

    fix profile

--- src/vue/components/profile.js
+++ src/vue/components/profile.js
@@ -84,7 +84,7 @@ Vue.component('change',{
                 icon: this.currentIcon,
                 iconColor: this.currentIconColor,
             };
-            this.$root.user.profileImg= this.profileImg;
+            this.$root.user.profileImg = profileImg;
             console.log(profileImg);
             console.log(this.$root.user);
             console.log(this.$root.user.profileImg);
@@ -127,9 +127,12 @@ Vue.component('profile', {
     props: ["user", "page","changeImg"],
     data(){
         return {
-            currentBG: this.user.profileImg.currentBG,
-            currentIcon: this.user.profileImg.currentIcon,
-            currentIconColor: this.user.profileImg.currentIconColor
+            profileImg:{
+                iconBG: this.user.profileImg.iconBG,
+                icon: this.user.profileImg.icon,
+                iconColor: this.user.profileImg.iconColor
+            }
+            
         }
     },
    
