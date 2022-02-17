fbbb36536fee8b8df2d6bc2013da328b10eca6be
Adnan <aaarras@ibadia.cat>
2022-02-17 20:52:55 +0100

    nivel 2 con sombra

--- phasernivel2/scenes/Game.js
+++ phasernivel2/scenes/Game.js
@@ -20,6 +20,7 @@ class Game extends Phaser.Scene {
         for(let i=0; i<9; i++){
             this.load.image('simbol'+i, path+'assets/passwd/simbol'+i+'.png');
         }
+        this.load.image('mask', 'assets/mask1.png');
     }
 
     create() {
@@ -52,7 +53,21 @@ class Game extends Phaser.Scene {
         // this.wallsLayer = this.map.createStaticLayer("walls", this.tileset)
 
         // this.wallsLayer.setDepth(1)
+        let rt = this.add.renderTexture(0, 0, 1000, 1000);
+        rt.depth = 20
+        window.rt = rt
+        rt.fill(0x000000);
 
+        this.spotlight = this.make.sprite({
+            x: 400,
+            y: 300,
+            key: "mask",
+            add: false
+        });
+
+        let mask = rt.createBitmapMask(this.spotlight)
+        mask.invertAlpha = true;
+        rt.setMask(mask);
         
 
         var end = this.physics.add.staticGroup();
@@ -219,5 +234,7 @@ class Game extends Phaser.Scene {
   
     update() {
         this.player.update()
+        this.spotlight.x = this.player.x;
+        this.spotlight.y = this.player.y;
     }
 }
