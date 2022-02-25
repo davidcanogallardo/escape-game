834e9d545ae9716a25c2752ce9a05b0e1af3d91f
Oscar Adell <oadell@ibadia.cat>
2022-02-25 16:19:47 +0100

    cambios generic mini game

--- demo_game/classes/GenericMiniGame.js
+++ demo_game/classes/GenericMiniGame.js
@@ -1,6 +1,21 @@
 class GenericMiniGame extends Phaser.Scene{
-    constructor(name){
+    constructor(name,type,difficulty){
         super(name);
+
+        //el tipo del minijuego solamente puede ser reto(challenge) o ayuda (helper)
+        if (type == 'challenge' ||type == 'helper') {
+            this.type = type;
+        } else {
+            console.log('El tipo del minijuego no es correcto');
+        }
+        
+        //Dificultad del minijuego
+        if (difficulty == 'easy' || difficulty == 'medium' || difficulty=='hard') {
+            this.difficulty = difficulty;
+        } else {
+            console.log('La dificultad introducida no es correcta');
+        }
+
         this.buttonActive;
         this.stickButtonActive;
         this.stickActive;
