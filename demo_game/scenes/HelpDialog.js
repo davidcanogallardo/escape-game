ab28e08207ca02d7a4da580ea14d109c1cc3d4bc
David <dcano@ibadia.cat>
2022-04-28 20:38:40 +0200

    ajustes tamaño ui y location reload al terminar partida

--- demo_game/scenes/HelpDialog.js
+++ demo_game/scenes/HelpDialog.js
@@ -13,21 +13,24 @@ class HelpDialog extends Phaser.Scene {
     }
 
     create() {
-        console.log(this);
+        window.helpd = this
         this.scene.bringToTop();
         console.log(this.helpMessage);
         let { width, height } = this.sys.game.canvas;
-        this.dialog = this.add.image(width/2, height/2, 'passwd_bg');
-        this.dialog.setDepth(56)
+        this.helpDialogFrame = this.add.image(
+            width/2, 
+            height/2, 
+            'passwd_bg'
+        ).setScale(1.4).setDepth(56);
         
-        var leftDialog = this.dialog.x - (this.dialog.displayWidth/2)
-        var topDialog = this.dialog.y - (this.dialog.displayHeight/2)+80
-        var bottomDialog = this.dialog.y + (this.dialog.displayHeight/2)-80
+        var leftDialog = this.helpDialogFrame.x - (this.helpDialogFrame.displayWidth/2)
+        var topDialog = this.helpDialogFrame.y - (this.helpDialogFrame.displayHeight/2)+80
+        var bottomDialog = this.helpDialogFrame.y + (this.helpDialogFrame.displayHeight/2)-80
 
-        this.awsd = this.add.image(0, this.dialog.y-20, 'awsd').setScale(0.6).setDepth(99);
-        this.blue = this.add.image(0, this.dialog.y-20, 'blue').setScale(0.6).setDepth(99);
-        this.red = this.add.image(0, this.dialog.y-20, 'red').setScale(0.6).setDepth(99);
-        this.green = this.add.image(0, this.dialog.y-20, 'green').setScale(0.6).setDepth(99);
+        this.awsd = this.add.image(0, this.helpDialogFrame.y-20, 'awsd').setScale(0.6).setDepth(99);
+        this.blue = this.add.image(0, this.helpDialogFrame.y-20, 'blue').setScale(0.6).setDepth(99);
+        this.red = this.add.image(0, this.helpDialogFrame.y-20, 'red').setScale(0.6).setDepth(99);
+        this.green = this.add.image(0, this.helpDialogFrame.y-20, 'green').setScale(0.6).setDepth(99);
 
         // Guardo los botones en un array de arrays con el 
         // boton y el texto que tendrá debajo
@@ -39,7 +42,7 @@ class HelpDialog extends Phaser.Scene {
         ]
 
         // Distancia en px entre imagen e imagen
-        var pxDistance = this.dialog.displayWidth/buttons.length
+        var pxDistance = this.helpDialogFrame.displayWidth/buttons.length
 
         // itero los botones para colocarlos
         for (let i = 0; i < buttons.length; i++) {
@@ -48,15 +51,15 @@ class HelpDialog extends Phaser.Scene {
             var xPos = leftDialog+(i*pxDistance)+(distanceBetweenImg/2)
             button[0].x = xPos
 
-            var text = this.add.text(xPos,this.dialog.y+30, button[1], {
+            var text = this.add.text(xPos,this.helpDialogFrame.y+30, button[1], {
                 fontSize: 25,
                 fontFamily: 'sans'
             }).setDepth(99)
             text.x -= text.displayWidth/2
         }
 
-        var text = this.add.text(this.dialog.x,this.dialog.y+70, this.helpMessage, {
-            fontSize: 25,
+        var text = this.add.text(this.helpDialogFrame.x,this.helpDialogFrame.y+90, this.helpMessage, {
+            fontSize: 29,
             fontFamily: 'sans'
         }).setDepth(99)
         text.x -= text.displayWidth/2
@@ -67,7 +70,6 @@ class HelpDialog extends Phaser.Scene {
 
         })
         this.scene.get('ui').events.on('quit', this.quit, this);
-        
     }
 
     quit() {
